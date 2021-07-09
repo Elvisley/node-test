d1b370df9a1e2381054631803d2dd8e688604375
Elvis <elvisley.pereira@luizalabs.com>
2021-07-09 10:33:56 -0300

    first commit

---
+++ node/app/server.js
@@ -0,0 +1,25 @@
+'use strict';
+
+// Add this to the VERY top of the first file loaded in your app
+const apm = require('elastic-apm-node').start({
+    // Override service name from package.json
+    // Allowed characters: a-z, A-Z, 0-9, -, _, and space
+    serviceName: 'NodeApp',
+    // Set custom APM Server URL (default: http://localhost:8200)
+    serverUrl: 'http://apm-server:8200',
+  })
+
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
