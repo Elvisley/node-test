53e442a8ad54445677db136fb457d28cd2e32e3f
Elvis <elvisley.pereira@luizalabs.com>
2021-07-09 12:54:04 -0300

    alt

--- node/app/server.js
+++ node/app/server.js
@@ -1,12 +1,22 @@
 'use strict';
 
+require('dotenv/config');
+
+if (!process.env.APM_SERVICE_NAME) {
+  console.log('APM_SERVICE_NAME not exist')
+}
+
+if (!process.env.APM_SERVER_URL) {
+  console.log('APM_SERVER_URL not exist')
+}
+
 // Add this to the VERY top of the first file loaded in your app
 const apm = require('elastic-apm-node').start({
     // Override service name from package.json
     // Allowed characters: a-z, A-Z, 0-9, -, _, and space
-    serviceName: 'NodeApp',
+    serviceName: process.env.APM_SERVICE_NAME,
     // Set custom APM Server URL (default: http://localhost:8200)
-    serverUrl: 'http://apm-server:8200',
+    serverUrl: process.env.APM_SERVER_URL,
   })
 
 const express = require('express');
